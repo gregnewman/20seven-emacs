7321a6b219effe879ace29e496418eba2b96c538
Greg Newman <greg@20seven.org>
2009-01-05 07:55:41 -0500

    switching theme to twilight

--- configs/cfg_theme.el
+++ configs/cfg_theme.el
@@ -4,6 +4,8 @@
 
 ;(color-theme-blippblopp)
 ;(color-theme-inkpot)
-(load-file "~/.emacs.d/vendor/color-theme/color-theme-tango-2.el")
-(color-theme-tango-2)
+;(load-file "~/.emacs.d/vendor/color-theme/color-theme-tango-2.el")
+(load-file "~/.emacs.d/vendor/color-theme/twilight-emacs/color-theme-twilight.el")
+;(color-theme-tango-2)
+(color-theme-twilight)
 ;(color-theme-montz)
