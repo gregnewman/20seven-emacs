1021a57b62e67679ff3ba06e0087c3d280b0d620
Greg Newman <greg@20seven.org>
2009-06-14 19:27:34 -0400

    fixing org

--- configs/org.el
+++ configs/org.el
@@ -5,29 +5,11 @@
 (add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
 (require 'org-install)
 
-(setq message-mode-hook
-      (quote (orgstruct++-mode
-              (lambda nil (setq fill-column 72) (flyspell-mode 1))
-              turn-on-auto-fill
-              bbdb-define-all-aliases)))
 ;;
 ;; Standard key bindings
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
-
-;; Make TAB the yas trigger key in the org-mode-hook and turn on flyspell mode
-(add-hook 'org-mode-hook
-          (lambda ()
-            ;; yasnippet
-            (make-variable-buffer-local 'yas/trigger-key)
-            (setq yas/trigger-key [tab])
-            (define-key yas/keymap [tab] 'yas/next-field-group)
-            ;; flyspell mode to spell checking everywhere
-            (flyspell-mode 1)))
-
-
-
 
 (setq org-agenda-files (quote ("~/hgfiles/org/tasks.org"
                                "~/hgfiles/org/notes.org"
@@ -40,12 +22,31 @@
                                "~/hgfiles/org/mind.org"
                                "~/hgfiles/org/archive.org")))
 
+(defun gtd ()
+   (interactive)
+   (find-file "~/hgfiles/org/todo.org")
+)
+
+;;(setq message-mode-hook
+;;      (quote (orgstruct++-mode
+;;              (lambda nil (setq fill-column 72) (flyspell-mode 1))
+;;              turn-on-auto-fill
+;;              bbdb-define-all-aliases)))
 
 
+;; Make TAB the yas trigger key in the org-mode-hook and turn on flyspell mode
+(add-hook 'org-mode-hook
+          (lambda ()
+            ;; yasnippet
+            (make-variable-buffer-local 'yas/trigger-key)
+            (setq yas/trigger-key [tab])
+            (define-key yas/keymap [tab] 'yas/next-field-group)))
+            ;; flyspell mode to spell checking everywhere
+;;            (flyspell-mode 1)))
 
 (setq org-todo-keywords (quote ((sequence "TODO(t)" "STARTED(s)" "|" "DONE(d!/!)")
-                                (sequence "WAITING(w@/!)" "SOMEDAY(S!)" "PROJECT(P@)" "OPEN(O@") "|" "CANCELLED(c@/!)")))
-
+ (sequence "WAITING(w@/!)" "SOMEDAY(S!)" "PROJECT(P@)" "OPEN(O@)" "|" "CANCELLED(c@/!)")
+ (sequence "QUOTE(q!)" "QUOTED(Q!)" "|" "APPROVED(A@)" "EXPIRED(E@)" "REJECTED(R@)"))))
 
 (setq org-todo-keyword-faces (quote (("TODO" :foreground "red" :weight bold)
  ("STARTED" :foreground "blue" :weight bold)
@@ -70,8 +71,6 @@
 
 ;; Change task state to STARTED when clocking in
 (setq org-clock-in-switch-to-state "STARTED")
-
-
 
 ;;
 ;;;  Load Org Remember Stuff
@@ -236,8 +235,6 @@
 (load "~/.emacs.d/vendor/org-mode/contrib/lisp/org-checklist")
 ;; to use it in a task you simply set the property RESET_CHECK_BOXES to t like this
 
+;; save all org files every minute
+(run-at-time "00:59" 3600 'org-save-all-org-buffers)
 
-(defun gtd ()
-   (interactive)
-   (find-file "~/hgfiles/org/todo.org")
-)
