e74e1e8782628e7164afceddfba1bc5c88773aa3
Greg Newman <greg@20seven.org>
2009-09-28 05:20:26 -0400

    fix yasnippet

--- configs/yasnippet.el
+++ configs/yasnippet.el
@@ -5,3 +5,9 @@
       'yas/dropdown-list-popup-for-template)
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets/")
+
+(add-hook 'org-mode-hook
+          #'(lambda ()
+              (setq yas/fallback-behavior
+                    `(apply ,(lookup-key org-mode-map [tab])))
+              (local-set-key [tab] 'yas/expand)))
