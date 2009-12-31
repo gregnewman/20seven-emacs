c13eb92fabe2ba3e0b0f3f044fda443b4ce301a0
Greg Newman <greg@20seven.org>
2009-12-30 20:13:57 -0500

    pep8 or 404?

--- vendor/pep8.py
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
-<html><head>
-<title>404 Not Found</title>
-</head><body>
-<h1>Not Found</h1>
-<p>The requested URL /trunk/devtools/pep8/pep8.py was not found on this server.</p>
-<hr>
-<address>Apache/2.2.3 (Debian) PHP/5.2.0-8+etch15 mod_ssl/2.2.3 OpenSSL/0.9.8c mod_perl/2.0.2 Perl/v5.8.8 Server at svn.browsershots.org Port 80</address>
-</body></html>
